65b69fa450900988f7b67cff33058b44c3b1ddec
Dexter2772 <31225973+Dexter2772@users.noreply.github.com>
2019-03-11 21:18:50 -0400

    Create difficulty method

--- game.py
+++ game.py
@@ -5,3 +5,13 @@ class Game():
 
     def __init__(self, difficulty):
         self.difficulty = difficulty
+
+    def ask_question(self):
+        if self.difficulty == 1:
+            pass
+        elif self.difficulty == 2:
+            pass
+        else:
+            pass
+
+    
